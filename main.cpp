0e10123ec48bd2a869aa505eafef2e864ce9b8fe
axell-brendow <breno.axel@gmail.com>
2020-11-28 21:16:51 -0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QGLFormat>
+
+int main(int argc, char *argv[])
+{
+    QGLFormat format = QGLFormat :: defaultFormat () ;
+    format.setSampleBuffers ( true );
+    format.setSamples (8) ;
+
+    if (! format . sampleBuffers () )
+        qWarning (" Multisample buffer is not supported .");
+
+    QGLFormat :: setDefaultFormat ( format );
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
